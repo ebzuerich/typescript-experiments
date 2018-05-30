7fd2a791c4503ff64e85f02f99dbdea4d54a5356
openscript <1105080+openscript@users.noreply.github.com>
2018-05-30 10:53:41 +0200

    Add oo example

---
+++ objects.ts
@@ -0,0 +1,39 @@
+interface Human {
+    firstName: string;
+    lastName: string;
+    name?: Function;
+    isLate?(time: Date): Function;
+}
+  
+class Person implements Human {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    name() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    whoAreYou() {
+        return `Hi I’m ${this.name()}`;
+    }
+}
+
+class Student extends Person {
+    course: string;
+
+    constructor(firstName: string, lastName: string, course: string) {
+        super(firstName, lastName);
+        this.course = course;
+    }
+
+    whoAreYou() {
+        return `${super.whoAreYou()} and I’m studying ${this.course}`;
+    }
+}
+let robin = new Student('Robin', 'Bühler', 'JavaScript');
+console.log(robin.whoAreYou());
